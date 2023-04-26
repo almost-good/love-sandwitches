321810b9b23fb812b8c9c813ba19d8aba9e6d71b
almost-good <marja.marinic@gmail.com>
2023-04-26 21:50:08 +0100

    Add validation to check for exactly 6 values in provided data

--- run.py
+++ run.py
@@ -17,8 +17,51 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 # Access data from the sheet
+'''
+The purpose of this code is to check if API is working.
+
 sales = SHEET.worksheet('sales')
 
 data = sales.get_all_values()
 
 print(data)
+'''
+
+
+def get_sales_data():
+    """
+    Get sales figures input from the user
+    """
+
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
+
+    data_str = input("Enter your data here: ")
+    # print(f"The data provided is {data_str}")
+
+    sales_data = data_str.split(",")
+    # print(sales_data)
+    validate_data(sales_data)
+
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if string cannot be converted into int, 
+    or if there aren't exactly 6 values.
+    """
+
+    # print(values)
+
+    try:
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+
+
+get_sales_data()
+# After this check if the data is valid
